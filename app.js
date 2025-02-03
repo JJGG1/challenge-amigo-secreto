1785a828aa964045b21e1326107aa7df4a53f672
JJGG1 <gud.josep@gmail.com>
2025-02-02 20:53:40 -0600

    Valida la entrada

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ let amigos = [];
 function agregarAmigo(){
     // Capturar el valor del campo de entrada
     let nombre = document.getElementById('amigo').ariaValueMax.trim();
+    // Validar la entrada
+    if (nombre === ""){
+        alert("Por favor, inserte un nombre");
+    }
 }
